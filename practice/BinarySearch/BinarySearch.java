de053b4dd69022540c6b7b00a40daf411bcd48dd
Aliaksei <kulevetsav@gmail.com>
2023-08-19 14:35:33 +0200

    BinarySearch

---
+++ practice/BinarySearch/BinarySearch.java
@@ -0,0 +1,60 @@
+package practice.BinarySearch;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(-3);
+        arrayList.add(2);
+        arrayList.add(8);
+        arrayList.add(0);
+        arrayList.add(10);
+        arrayList.add(-100);
+        arrayList.add(100);
+        Collections.sort(arrayList); // сортирока массива по возрастанию [-100, -3, 0, 2, 8, 10, 100]
+        System.out.println(arrayList);
+        Collections.reverse(arrayList); // развернуть массив на оборот [100, 10, 8, 2, 0, -3, -100]
+        System.out.println(arrayList);
+        Collections.shuffle(arrayList); // мешать массив [8, -3, 2, 10, 100, 0, -100]
+        System.out.println(arrayList);
+
+
+//        int index = Collections.binarySearch(arrayList,8);
+//        System.out.println(index);
+
+
+//        Employee em1 = new Employee(100,"Ivan", 3900);
+//        Employee em2 = new Employee(15,"Petr", 5400);
+//        Employee em3 = new Employee(123,"Marina", 3500);
+//        Employee em4 = new Employee(15,"Kolya", 6200);
+//        Employee em5 = new Employee(182,"Sasha", 4800);
+//        Employee em6 = new Employee(15,"Bogdan", 5000);
+//        Employee em7 = new Employee(250,"Aliaksei", 5500);
+//        List<Employee> employeeList = new ArrayList<>();
+//        employeeList.add(em1);
+//        employeeList.add(em2);
+//        employeeList.add(em3);
+//        employeeList.add(em4);
+//        employeeList.add(em5);
+//        employeeList.add(em6);
+//        employeeList.add(em7);
+//        System.out.println(employeeList);
+//        Collections.sort(employeeList);
+//        System.out.println(employeeList);
+//        int index = Collections.binarySearch(employeeList, new Employee(15,"Kolya", 0));
+//        System.out.println(index);
+
+
+//        int[] array = {-3,2,5,6,10,45,0};
+//        System.out.println(Arrays.toString(array));
+//        Arrays.sort(array);
+//        System.out.println(Arrays.toString(array));
+//        int index = Arrays.binarySearch(array,6);
+//        System.out.println(index);
+    }
+}
---
+++ practice/BinarySearch/Employee.java
@@ -0,0 +1,31 @@
+package practice.BinarySearch;
+
+public class Employee implements Comparable<Employee>{
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee anotherEmp) {
+        int result = this.id - anotherEmp.id;
+        if (result == 0) {
+            result = this.name.compareTo(anotherEmp.name);
+        }
+        return result;
+    }
+}
