ff983974901472ae6e86f1ec0c5b2cd876c2d71b
Aliaksei <kulevetsav@gmail.com>
2023-08-21 17:30:38 +0200

    Algorithms

--- practice/some/Algoritms/Average.java
+++ practice/some/Algoritms/Average.java
@@ -4,8 +4,8 @@ public class Average {
     public static void main(String[] args) {
         double[] array = {10.1, 11.2, 12.3, 13.4, 14.5};
         double result = 0;
-        for (int i = 0; i < array.length; i++) {
-            result += array[i];
+        for (double v : array) {
+            result += v;
         }
         System.out.println(result / array.length);
     }
