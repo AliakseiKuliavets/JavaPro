b8f8e2d23625b829139142c2f109437280c0da25
Aliaksei <kulevetsav@gmail.com>
2023-08-23 19:48:03 +0200

    TwoSum

--- practice/leetcode/TwoSum/TwoSum.java
+++ practice/leetcode/TwoSum/TwoSum.java
@@ -34,16 +34,75 @@ public class TwoSum {
         System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 
+    // time -> O(n), memory O(1)
     public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> numIndexMap = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (numIndexMap.containsKey(complement)) {
-                return new int[]{numIndexMap.get(complement), i};
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{nums[left],nums[right]};
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
             }
-            numIndexMap.put(nums[i], i);
         }
-        return new int[]{-1, -1};
+        return new int[]{};
+
+    // time -> O(n^2), memory O(n)
+    //    public static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    return new int[]{nums[i], nums[j]};
+//                }
+//            }
+//        }
+//        return new int[]{};
+//    }
+
+
+    // time -> O(log n), memory O(n)
+//    public static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i < nums.length; i++) {
+//            int complement = target - nums[i];
+//            int index = binarySearch(nums, complement, i + 1);
+//            if (index != -1) {
+//                return new int[]{nums[i], complement};
+//            }
+//        }
+//        return new int[]{};
+//    }
+//    public static int binarySearch(int[] nums, int target, int start) {
+//        int left = start;
+//        int right = nums.length - 1;
+//
+//        while (left <= right) {
+//            int mid = left + (right - left) / 2;
+//            if (nums[mid] == target) {
+//                return mid;
+//            } else if (nums[mid] < target) {
+//                left = mid + 1;
+//            } else {
+//                right = mid - 1;
+//            }
+//        }
+//        return -1;
+//    }
+
+
+        // time -> O(n), memory O(n)
+//    public static int[] twoSum(int[] nums, int target) {
+//        Map<Integer, Integer> numIndexMap = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++) {
+//            int complement = target - nums[i];
+//            if (numIndexMap.containsKey(complement)) {
+//                return new int[]{numIndexMap.get(complement), i};
+//            }
+//            numIndexMap.put(nums[i], i);
+//        }
+//        return new int[]{};
     }
 }
 
