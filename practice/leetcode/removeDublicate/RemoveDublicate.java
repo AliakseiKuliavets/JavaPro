a667e4cb8ef3f7a8f8ce474f75349f015cd1c9c1
Aliaksei <kulevetsav@gmail.com>
2023-08-23 20:10:27 +0200

    TwoSum

--- practice/leetcode/removeDublicate/RemoveDublicate.java
+++ practice/leetcode/removeDublicate/RemoveDublicate.java
@@ -1,22 +1,31 @@
 package practice.leetcode.removeDublicate;
 
+import java.util.Arrays;
+
 public class RemoveDublicate {
     public static void main(String[] args) {
-
+        int[] nums = {-11, -2, 2, 7, 11, 15, 19, 2, 5, 3};
+        int[] uniqueArray = removeDuplicates(nums);
+        System.out.println(Arrays.toString(uniqueArray));
     }
 
-    public int removeDuplicates(int[] nums) {
-       if (nums.length == 0) {
-           return 0;
-       }
-       int uniqueCount = 1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != nums[uniqueCount-1]) {
-                nums[uniqueCount] = nums[i];
-                uniqueCount++;
+    // time -> O(n log n), memory O(1)
+    public static int[] removeDuplicates(int[] nums) {
+        if (nums.length <= 1) {
+            return nums;
+        }
+
+        Arrays.sort(nums);
+
+        int countNum = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[countNum] = nums[i];
+                countNum++;
             }
         }
-        return uniqueCount;
-    }
 
+        return Arrays.copyOf(nums, countNum);
+    }
 }
+
