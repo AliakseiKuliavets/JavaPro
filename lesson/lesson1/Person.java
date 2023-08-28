82c4ee949de42f3d4c6f9a441d5090c5b3d2e934
Aliaksei <kulevetsav@gmail.com>
2023-08-28 12:48:16 +0200

    lesson1

---
+++ lesson/lesson1/Person.java
@@ -0,0 +1,10 @@
+package lesson.lesson1;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
