ac4cda973927d42d3d096c060cd847320ff2bd8d
Aliaksei <kulevetsav@gmail.com>
2023-08-28 12:08:53 +0200

    lesson1

---
+++ lesson/lesson1/Main3.java
@@ -0,0 +1,21 @@
+package lesson.lesson1;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int[] array = {2,4,3,2};
+        System.out.println(new Main3().sumArray(array));
+    }
+    private int sumArray(int[] array) {
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                sumEven = sumEven + array[i];
+            } else {
+                sumOdd = sumOdd + array[i];
+            }
+        }
+        return sumEven - sumOdd;
+    }
+}
---
+++ lesson/lesson1/Main4.java
@@ -0,0 +1,29 @@
+package lesson.lesson1;
+
+public class Main4 {
+    public static void main(String[] args) {
+        int[] array = {1, 3, 2,3};
+        System.out.println(new Main4().sumArray(array));
+    }
+
+    private int sumArray(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        }
+        int left = 0;
+        int right = array.length - 1;
+        int sum = 0;
+
+        while (left <= right) {
+            if (left == right) {
+                sum += array[left];
+                break;
+            } else {
+                sum = sum + array[left] + array[right];
+                left++;
+                right--;
+            }
+        }
+        return sum;
+    }
+}
