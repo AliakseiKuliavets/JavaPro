a8519990967463c20added6c47796791d3778432
Aliaksei <kulevetsav@gmail.com>
2023-08-28 10:46:17 +0200

    lesson1

--- lesson/lesson1/Main1.java
+++ lesson/lesson1/Main1.java
@@ -2,6 +2,18 @@ package lesson.lesson1;
 
 public class Main1 {
     public static void main(String[] args) {
+        int[] array = {12,445,678,9};
 
+        System.out.println(new Main1().returnMin(array));
+    }
+    private int returnMin(int[] array) {
+        int min = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            min = array[i];
+            if (min > array[i+1]){
+                min = array[i+1];
+            }
+        }
+        return min;
     }
 }
