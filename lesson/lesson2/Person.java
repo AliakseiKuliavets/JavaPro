57e1c7f3811bb9582bbb14c9115224ccd868d76c
Aliaksei <kulevetsav@gmail.com>
2023-08-30 11:13:20 +0200

    lesson2

--- lesson/lesson2/Person.java
+++ lesson/lesson2/Person.java
@@ -1,10 +1,16 @@
 package lesson.lesson2;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Person {
-    int age;
-    String name;
-//    Dog dog;
-    Animal animal;
+    private int age = 35;
+    private String name = "Bob";
+    //    Dog dog;
+    private Animal animal;
+
     public Person() {
     }
 
@@ -14,23 +20,46 @@ public class Person {
         this.animal = animal;
     }
 
+    @Override
+    public String toString() {
+        return "Person{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", animal=" + animal +
+                '}';
+    }
+
     public static void main(String[] args) {
         Dog dog1 = new Dog("Bob");
-        Person person = new Person(25,"Nmae", dog1);
+        Person person = new Person(25, "Nmae", dog1);
+        Person person1 = new Person();
+        System.out.println(person);
+        System.out.println(person1);
+        Animal dog = new Dog("Tuzik");
+        Person per = new Person(29, "Mike", dog);
+
     }
 }
 
-class Animal{
+class Animal {
 
 }
-class Dog extends Animal{
-    String name;
+
+class Dog extends Animal {
+    private String name;
 
     public Dog(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
 
-class Cat extends Animal{
-    String name;
+class Cat extends Animal {
+    private String name;
 }
