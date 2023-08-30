114e711f3c09c4613a77e1d8f74cb81d1ac478b7
Aliaksei <kulevetsav@gmail.com>
2023-08-30 10:50:42 +0200

    lesson2

--- lesson/lesson2/Person.java
+++ lesson/lesson2/Person.java
@@ -1,4 +1,36 @@
 package lesson.lesson2;
 
 public class Person {
+    int age;
+    String name;
+//    Dog dog;
+    Animal animal;
+    public Person() {
+    }
+
+    public Person(int age, String name, Animal animal) {
+        this.age = age;
+        this.name = name;
+        this.animal = animal;
+    }
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Bob");
+        Person person = new Person(25,"Nmae", dog1);
+    }
+}
+
+class Animal{
+
+}
+class Dog extends Animal{
+    String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+}
+
+class Cat extends Animal{
+    String name;
 }
