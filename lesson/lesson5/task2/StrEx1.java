b6caf861d3c14cd32493abbfa57db22dbb9a5e1e
Aliaksei <kulevetsav@gmail.com>
2023-09-11 12:28:05 +0200

    lesson5

---
+++ lesson/lesson5/task2/StrEx1.java
@@ -0,0 +1,10 @@
+package lesson.lesson5.task2;
+
+public class StrEx1 {
+    public static void main(String[] args) {
+        String nik = "1";
+        char nik2 = '1';
+
+        System.out.println();
+    }
+}
