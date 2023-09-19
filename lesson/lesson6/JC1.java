7f2b9809377c0f0997dc65bdae07787baa227f2a
Aliaksei <kulevetsav@gmail.com>
2023-09-19 12:19:13 +0200

    lesson6

---
+++ lesson/lesson6/JC1.java
@@ -0,0 +1,10 @@
+package lesson.lesson6;
+
+
+public class JC1 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int[] q = {1,2,34,56,7};
+    }
+}
---
+++ lesson/lesson6/JC2.java
@@ -0,0 +1,10 @@
+package lesson.lesson6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JC2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(20);
+    }
+}
