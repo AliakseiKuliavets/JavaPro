2257defd37d73122636f75bc07f55ff5c6a400ea
Aliaksei <kulevetsav@gmail.com>
2023-09-25 13:14:45 +0200

    lesson9

---
+++ lesson/lesson9/lesson/Ex/Animal.java
@@ -0,0 +1,45 @@
+package lesson.lesson9.lesson.Ex;
+
+import java.util.Objects;
+
+public class Animal implements Comparable<Animal>{
+    private int age;
+    private String name;
+    private boolean sheSwims;
+    private char bloodType;
+
+    public Animal(int age, String name, boolean sheSwims, char bloodType) {
+        this.age = age;
+        this.name = name;
+        this.sheSwims = sheSwims;
+        this.bloodType = bloodType;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", sheSwims=" + sheSwims +
+                ", bloodType=" + bloodType +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        return this.name.compareTo(o.name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
---
+++ lesson/lesson9/lesson/Ex/Main.java
@@ -0,0 +1,35 @@
+package lesson.lesson9.lesson.Ex;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal(20,"Camel",true,'A');
+        Animal animal2 = new Animal(25,"Shlange",false,'A');
+        Animal animal3 = new Animal(2,"Horse",false,'B');
+        Animal animal4 = new Animal(22,"Eagle",false,'A');
+        Animal animal5 = new Animal(28,"Insekte",true,'C');
+        Animal animal6 = new Animal(2,"Cat",false,'A');
+        Animal animal7 = new Animal(3,"Dog",true,'A');
+        Animal animal8 = new Animal(1,"Frog",true,'F');
+        Animal animal9 = new Animal(1,"Spider",false,'B');
+        Animal animal10 = new Animal(7,"Bird",false,'C');
+        Animal animal11 = new Animal(7,"Bird",false,'B');
+
+        Set<Animal> animalSet = new HashSet<>();
+        animalSet.add(animal1);
+        animalSet.add(animal2);
+        animalSet.add(animal3);
+        animalSet.add(animal4);
+        animalSet.add(animal5);
+        animalSet.add(animal6);
+        animalSet.add(animal7);
+        animalSet.add(animal8);
+        animalSet.add(animal9);
+        animalSet.add(animal10);
+        animalSet.add(animal11);
+
+        System.out.println(animalSet);
+    }
+}
