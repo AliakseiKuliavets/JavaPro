c8bab7044d8aecef1de4248e21ac2dde53f140b6
Aliaksei <kulevetsav@gmail.com>
2023-10-02 10:31:56 +0200

    21

--- homeWork/_21/Main.java
+++ homeWork/_21/Main.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Cards cards = new Cards();
-        System.out.println(cards.createCards());
-        Bankir bankir = new Bankir(10_000,"Bankir", null,20);
-
+//        Cards cards = new Cards();
+//        System.out.println(cards.createCards());
+//        Bankir bankir = new Bankir(10_000,"Bankir", null,20);
+//
         Gamer gamer1 = new Gamer("First", 1000, null,0);
-        List<Gamer> gamerList = new ArrayList<>();
-        gamerList.add(gamer1);
-
-        GameManagment gameManagment = new GameManagment(gamerList,bankir);
-        gameManagment.addCardGamer(gamer1);
-        System.out.println(gamer1);
+//        List<Gamer> gamerList = new ArrayList<>();
+//        gamerList.add(gamer1);
+//
+//        GameManagment gameManagment = new GameManagment(gamerList,bankir);
+//        gameManagment.addCardGamer(gamer1);
+//        System.out.println(gamer1);
         LogicGame logicGame = new LogicGame();
         logicGame.startGame(gamer1);
     }
