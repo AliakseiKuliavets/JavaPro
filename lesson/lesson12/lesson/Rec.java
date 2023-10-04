0bf65453d0a9dab2396f4ae8c63204410efd4f1a
Aliaksei <kulevetsav@gmail.com>
2023-10-04 10:33:56 +0200

    lesson12

--- lesson/lesson12/lesson/Rec.java
+++ lesson/lesson12/lesson/Rec.java
@@ -2,15 +2,19 @@ package lesson.lesson12.lesson;
 
 public class Rec {
     public static void main(String[] args) {
-
+        m1(Rec::rr);
     }
     public static void rr() {
         System.out.println("12341");
         System.out.println("/////");
         System.out.println("@@@@@");
     }
+    public static void m1(Inter1 inter1) {
+        System.out.println("###########");
+        inter1.get();
+    }
 }
 @FunctionalInterface
 interface Inter1 {
-    void run();
+    void get();
 }
