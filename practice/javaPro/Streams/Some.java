a2207aba990d34b6c694704a752a33fa08d202e3
Aliaksei <kulevetsav@gmail.com>
2023-10-06 22:54:24 +0200

    Streams

---
+++ practice/javaPro/Streams/Some.java
@@ -0,0 +1,11 @@
+package practice.javaPro.Streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Some {
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("Privet","Kak dela", "Okey" , "Poka");
+
+    }
+}
