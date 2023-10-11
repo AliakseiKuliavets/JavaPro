68b5fc341fa3a954e417b30aefaf929634e52813
Aliaksei <kulevetsav@gmail.com>
2023-10-11 10:56:08 +0200

    lesson14

--- lesson/lesson14/FibEx.java
+++ lesson/lesson14/FibEx.java
@@ -1,5 +1,7 @@
 package lesson.lesson14;
 
+import java.util.stream.Stream;
+
 public class FibEx {
     public static long fibLoop(final int index) {
         if (index < 0) {
@@ -15,5 +17,42 @@ public class FibEx {
             fib2 = (int) fibCurrent;
         }
         return fib2;
+    }
+
+    public static long fibRec(final int index) {
+        if (index < 0) {
+            throw new IllegalArgumentException();
+        }
+        if (index == 0) {
+            return 0;
+        }
+
+        return index == 1 || index == 2 ? 1L : fibRec(index - 2) + fibRec(index - 1);
+    }
+
+    public static long fibStream(final int index){
+       var result = Stream.iterate(new long[]{0,1} , arr -> new long[]{arr[1], arr[0] + arr[1]})
+               .limit(index+1)
+               .map(y -> y[0])
+               .max(Long::compareTo);
+       return result.orElseThrow(IllegalAccessError::new);
+    }
+
+    public static void main(String[] args) {
+        int n = 50;
+        long fibLoop = System.currentTimeMillis();
+        fibLoop(n);
+        long fibLoop2 = System.currentTimeMillis();
+        System.out.println("Time1: " + (fibLoop2 - fibLoop));
+
+        fibLoop = System.currentTimeMillis();
+        fibRec(n);
+        fibLoop2 = System.currentTimeMillis();
+        System.out.println("Time2: " + (fibLoop2 - fibLoop));
+
+        fibLoop = System.currentTimeMillis();
+        fibStream(n);
+        fibLoop2 = System.currentTimeMillis();
+        System.out.println("Time3: " + (fibLoop2 - fibLoop));
     }
 }
