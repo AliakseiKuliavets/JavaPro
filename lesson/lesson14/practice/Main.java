7a8b39bcbf4db2c674dfbaf4c4800fa4b8e4b99c
Aliaksei <kulevetsav@gmail.com>
2023-10-11 19:45:43 +0200

    lesson14

--- lesson/lesson14/practice/Main.java
+++ lesson/lesson14/practice/Main.java
@@ -2,13 +2,9 @@ package lesson.lesson14.practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.OptionalInt;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("Java", "JavaScript", "Python", "Ruby");
-        List<Integer> list2 = Arrays.asList(10, 15, 20, 25, 30);
-
         Methods.m1();
         Methods.m2();
         Methods.m3();
@@ -18,6 +14,5 @@ public class Main {
         Methods.m7();
         Methods.m8();
         Methods.m9();
-
     }
 }
