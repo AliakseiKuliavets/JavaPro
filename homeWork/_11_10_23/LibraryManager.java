a781b0baddd4b2505f1c4fe190370fb6e785428b
Aliaksei <kulevetsav@gmail.com>
2023-10-14 19:10:50 +0200

    HM_11_10_23

--- homeWork/_11_10_23/LibraryManager.java
+++ homeWork/_11_10_23/LibraryManager.java
@@ -1,4 +1,10 @@
 package homeWork._11_10_23;
 
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+@AllArgsConstructor
 public class LibraryManager {
+    private List<Book> bookList;
+    private List<LibraryUser> libraryUsers;
 }
