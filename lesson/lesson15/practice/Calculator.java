431270f73ebe0969a013e0286d4404d6d66095f4
Aliaksei <kulevetsav@gmail.com>
2023-10-16 11:33:08 +0200

    hm16_10_23

---
+++ lesson/lesson15/practice/Calculator.java
@@ -0,0 +1,16 @@
+package lesson.lesson15.practice;
+
+public class Calculator<T extends Number> {
+    private T value;
+
+    public Calculator(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+    public double add(Calculator<? extends  Number> other){
+        return  this.value.doubleValue() + other.value.doubleValue();
+    }
+}
---
+++ lesson/lesson15/practice/Ge4.java
@@ -0,0 +1,10 @@
+package lesson.lesson15.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ge4 {
+    public static void main(String[] args) {
+        List<?> list = new ArrayList<>();
+    }
+}
