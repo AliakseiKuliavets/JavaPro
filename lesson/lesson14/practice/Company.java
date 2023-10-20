ec86b0e5ae90c1999665897221f9724be10a297f
Aliaksei <kulevetsav@gmail.com>
2023-10-20 11:55:08 +0200

    lesson14

--- lesson/lesson14/practice/Company.java
+++ lesson/lesson14/practice/Company.java
@@ -4,10 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
-@AllArgsConstructor
-@ToString
-@Getter
+
 public class Company {
     private String name;
     private String city;
+
+    public Company(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
--- lesson/lesson14/practice/Employee.java
+++ lesson/lesson14/practice/Employee.java
@@ -4,12 +4,43 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
-@AllArgsConstructor
-@ToString
-@Getter
+
 public class Employee {
     private String name;
     private int salary;
     private String job;
     private Company company;
+
+    public Employee(String name, int salary, String job, Company company) {
+        this.name = name;
+        this.salary = salary;
+        this.job = job;
+        this.company = company;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", job='" + job + '\'' +
+                ", company=" + company +
+                '}';
+    }
 }
