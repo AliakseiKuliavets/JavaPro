ab543bd72f7e8066a60a9a62ce381b2a63a8e708
Aliaksei <kulevetsav@gmail.com>
2023-10-20 11:52:49 +0200

    homework_05_10_23

--- homeWork/_05_10_23/Student.java
+++ homeWork/_05_10_23/Student.java
@@ -4,10 +4,37 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
-@AllArgsConstructor
-@ToString
-@Getter
 public class Student {
     private int age;
     private String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
