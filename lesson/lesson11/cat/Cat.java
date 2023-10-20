820ada05ec2d075a2ded16c16992e19151d23604
Aliaksei <kulevetsav@gmail.com>
2023-10-20 11:51:31 +0200

    lesson11

--- lesson/lesson11/cat/Cat.java
+++ lesson/lesson11/cat/Cat.java
@@ -5,11 +5,32 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Setter
-@Getter
-@AllArgsConstructor
-@ToString
 public class Cat {
     private int age;
     private final String name;
+
+    public Cat(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
