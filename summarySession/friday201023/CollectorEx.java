9f535a5916ff20e88960882931bb78b65fa566f5
Aliaksei <kulevetsav@gmail.com>
2023-10-20 10:23:10 +0200

    summarySession20_10_23

---
+++ summarySession/friday201023/CollectorEx.java
@@ -0,0 +1,28 @@
+package summarySession.friday201023;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class CollectorEx {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Tom","Anton","Edward","Anna","Anna");
+        List<String> list = names.stream()
+                .filter(el -> el.length() > 4)
+//                .toList(); - UNMODIFIEBLE
+//                .collect(Collectors.toList()); // - ArrayList по default но зависит от рандома
+                .collect(Collectors.toCollection(LinkedList::new)); // - LinkedList - явно и конкретно создаем LinkedList
+        System.out.println(list.getClass());
+        list.add("Vasya");
+        System.out.println(list);
+
+//        Set<String> collect = names.stream()
+////                .distinct() - если для List
+//                .collect(Collectors.toSet());
+        Set<String> collect2 = new HashSet<>(names);
+        System.out.println(collect2);
+
+        Map<String, Integer> collect = names.stream().collect(Collectors.toMap(s -> s, el-> 1, Integer::sum));
+        System.out.println(collect.getClass());
+        System.out.println(collect);
+    }
+}
