fb5d93f900fc64cf423dde936f2a9200b6e9ef7d
Aliaksei <kulevetsav@gmail.com>
2023-10-20 11:53:39 +0200

    lesson14

--- lesson/lesson14/practice/Person.java
+++ lesson/lesson14/practice/Person.java
@@ -4,11 +4,29 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
-@AllArgsConstructor
-@Getter
-@ToString
-public class Person {
-    private String name;
-    private int age;
 
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
