2b5978a12fb9a944b23cbd537bf8994c903697df
Aliaksei <kulevetsav@gmail.com>
2023-10-23 13:20:29 +0200

    lesson17

---
+++ lesson/lesson17/practice/Pair.java
@@ -0,0 +1,27 @@
+package lesson.lesson17.practice;
+
+public class Pair<L,S> {
+    private final L longKey;
+    private final S shortsValue;
+
+    public Pair(L longKey, S shortsValue) {
+        this.longKey = longKey;
+        this.shortsValue = shortsValue;
+    }
+
+    public L getLongKey() {
+        return longKey;
+    }
+
+    public S getShortsValue() {
+        return shortsValue;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "longKey=" + longKey +
+                ", shortsValue=" + shortsValue +
+                '}';
+    }
+}
