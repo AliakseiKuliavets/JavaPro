800fb11aabe3480d3d815c742ef6faa05aa9f38a
Aliaksei <kulevetsav@gmail.com>
2023-10-28 19:10:33 +0200

    hmw

--- homeWork/_23_10_23/luna/CardGenerate.java
+++ homeWork/_23_10_23/luna/CardGenerate.java
@@ -3,6 +3,7 @@ package homeWork._23_10_23.luna;
 import java.util.Random;
 
 public class CardGenerate {
+    private static final int years = 2023;
     Random random = new Random();
 
     public String returnNumberCard() {
@@ -10,14 +11,14 @@ public class CardGenerate {
     }
 
     private String generateNumberCard() {
-        return generateNumber() + generateDataCard() + generateCVVCard();
+        return generateNumber() + generateDataMonthsCard() + generateDataYearCard() + generateCVVCard();
     }
 
     private String generateNumber() {
         return String.valueOf(random.nextLong(1_000_000_000_000_000L, 10_000_000_000_000_000L));
     }
 
-    private String generateDataCard() {
+    private String generateDataMonthsCard() {
         String months;
         int numberOfMounts = random.nextInt(1, 13);
         if (numberOfMounts >= 1 && numberOfMounts < 10) {
@@ -25,8 +26,10 @@ public class CardGenerate {
         } else {
             months = String.valueOf(numberOfMounts);
         }
-        String year = String.valueOf(random.nextInt(4, 6) + 23);
-        return months + year;
+        return months;
+    }
+    private String generateDataYearCard(){
+        return String.valueOf(random.nextInt(4, 6) + ( CardGenerate.years- 2000));
     }
 
     private String generateCVVCard() {
--- homeWork/_23_10_23/luna/Luna.java
+++ homeWork/_23_10_23/luna/Luna.java
@@ -25,8 +25,7 @@ public class Luna {
         for (int i = 0; i < 16; i++) {
             char c = number.charAt(i);
             if (Character.isDigit(c)) {
-                int digit = Character.getNumericValue(c);
-                integerList.add(digit);
+                integerList.add(Character.getNumericValue(c));
             }
         }
         // просто для проверки можно удалить
@@ -35,16 +34,15 @@ public class Luna {
     }
 
     private  List<Integer> lunaMethods(List<Integer> integerList) {
-        List<Integer> newNumberList = new ArrayList<>(integerList);
         for (int i = 0; i < integerList.size(); i += 2) {
             if (integerList.get(i) * 2 > 9) {
-                newNumberList.set(i, (integerList.get(i) * 2) - 9);
+                integerList.set(i, (integerList.get(i) * 2) - 9);
             } else {
-                newNumberList.set(i, integerList.get(i) * 2);
+                integerList.set(i, integerList.get(i) * 2);
             }
         }
         // просто для проверки можно удалить
-        System.out.println(newNumberList + " sum: "+ (newNumberList.stream().mapToInt(Integer::intValue).sum()));
-        return newNumberList;
+        System.out.println(integerList + " sum: "+ (integerList.stream().mapToInt(Integer::intValue).sum()));
+        return integerList;
     }
 }
