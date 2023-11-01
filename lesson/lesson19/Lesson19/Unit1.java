c2a2c306ec0bd2e957934d2d6c4488328987d71a
Aliaksei Kuliavets <132812649+AliakseiKuliavets@users.noreply.github.com>
2023-11-01 22:00:42 +0100

    Add files via upload

---
+++ lesson/lesson19/Lesson19/Unit1.java
@@ -0,0 +1,27 @@
+package Lesson19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Unit1 {
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public List<Integer> getOnlyEven(int[] numbers) {
+        if (numbers == null) {
+            throw new NullPointerException();
+        }
+
+        List<Integer> evenList = new ArrayList<>();
+        for (int number : numbers) {
+            if (number % 2 == 0) {
+                evenList.add(number);
+            }
+        }
+        return evenList;
+    }
+}
