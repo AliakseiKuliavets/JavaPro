afb2d01c23f5a263ccfc112d792bae7d320aa7df
Aliaksei <kulevetsav@gmail.com>
2023-11-06 16:46:18 +0100

    lesson21 practice

--- src/test/java/lesson/lesson21/taski/OrderServiceTest.java
+++ src/test/java/lesson/lesson21/taski/OrderServiceTest.java
@@ -18,16 +18,20 @@ public class OrderServiceTest {
     @Mock
     private PaymentService paymentService;
 
+    private Order order;
+
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
+        order = new Order("ad", 20.2, false);
         orderService = new OrderService(paymentService);
     }
 
+    /**
+     * Тест метода placeOrder(Order order) который проверяет, изменяется ли статус платежа на True если платеж поступил
+     */
     @Test
     public void placeOrderTrue() {
-        Order order = new Order("ad", 20.2, false);
         Mockito.when(paymentService.processPayment(order)).thenReturn(true);
-
         boolean result = orderService.placeOrder(order);
 
         assertTrue(result);
@@ -35,11 +39,13 @@ public class OrderServiceTest {
 
         Mockito.verify(paymentService).processPayment(order);
     }
+
+    /**
+     * Тест метода placeOrder(Order order) который проверяет, изменяется ли статус платежа, если не платеж поступил
+     */
     @Test
     public void placeOrderFalse() {
-        Order order = new Order("ad", 20.2, false);
         Mockito.when(paymentService.processPayment(order)).thenReturn(false);
-
         boolean result = orderService.placeOrder(order);
 
         Assertions.assertFalse(result);
