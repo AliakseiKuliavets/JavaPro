2f877636149d89a2edd0f94cffa855769dac85a0
Aliaksei <kulevetsav@gmail.com>
2023-11-06 11:21:49 +0100

    lesson21

--- src/test/java/lesson/lesson21/mockEx/SimpleTradingServiceTest.java
+++ src/test/java/lesson/lesson21/mockEx/SimpleTradingServiceTest.java
@@ -1,12 +1,14 @@
 package lesson.lesson21.mockEx;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 
-
+@ExtendWith(MockitoExtension.class)
 class SimpleTradingServiceTest {
     @Mock
     private AuditService auditService;
@@ -18,7 +20,9 @@ class SimpleTradingServiceTest {
     @Test
     public void logNewTradeWithMockTest(){
         Mockito.when(tradeRepository.createTrade(trade)).thenReturn(anyLong());
-
+        TradingService tradingService = new SimpleTradingService(tradeRepository,auditService);
+        tradingService.createTrade(trade);
+        Mockito.verify(auditService).logNewTrade(trade);
     }
     @Test
     public void createTrade() {
