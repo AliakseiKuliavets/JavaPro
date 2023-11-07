1239ae54f156c9e3417757ce2e4cdefd73e63a43
Aliaksei <kulevetsav@gmail.com>
2023-11-07 10:55:39 +0100

    lesson21 HMW

--- src/test/java/lesson/lesson21/taski/OrderFulfillmentServiceTest.java
+++ src/test/java/lesson/lesson21/taski/OrderFulfillmentServiceTest.java
@@ -2,7 +2,6 @@ package lesson.lesson21.taski;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
