e58492511b526ff2c700bd7e938d30f90b16b301
Aliaksei <kulevetsav@gmail.com>
2023-11-08 16:45:49 +0100

    lesson22

--- src/main/java/lesson/lesson22/taski/Methods.java
+++ src/main/java/lesson/lesson22/taski/Methods.java
@@ -1,10 +1,12 @@
 package lesson.lesson22.taski;
 
 
+import java.util.*;
+
 public class Methods {
     public static void main(String[] args) {
         String s1 = "aa";
-        String s2 = "aa##aa##";
+        String s2 = "##aaaa##";
         System.out.println(twoStringEquals(s1, s2));
     }
 
@@ -29,5 +31,110 @@ public class Methods {
 
     public static boolean twoStringEquals(String s1, String s2) {
         return count(s1).equals(count(s2));
+    }
+
+    static String toCamelCase(String s) {
+        if (s == null) {
+            throw new NullPointerException();
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '-' || s.charAt(i) == '_') {
+                String c = String.valueOf(s.charAt(i + 1));
+                result.append(c.toUpperCase());
+                i++;
+            } else {
+                result.append(s.charAt(i));
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    static int sum = 0;
+
+    public static int digital_root(int n) {
+        int sum2 = 0;
+        int temp = n;
+        while (temp != 0) {
+            sum2 = sum2 + temp % 10;
+            temp = temp / 10;
+        }
+        sum = sum2;
+        if (sum2 > 9) {
+            digital_root(sum);
+        }
+        return sum;
+    }
+
+    static String encode(String word) {
+        word = word.toLowerCase(); // Ignore capitalization
+        Map<Character, Integer> characterMap = new HashMap<>();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            characterMap.put(c, characterMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (characterMap.get(c) > 1) {
+                result.append(')');
+            } else {
+                result.append('(');
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static String order(String words) {
+        if (words == null) {
+            throw new IllegalArgumentException();
+        }
+        if (words.equals("")) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        String[] stringList = words.split("\\s");
+        String[] arrayString = new String[stringList.length];
+        for (int i = 0; i < stringList.length; i++) {
+            for (int j = 0; j < stringList[i].length(); j++) {
+                if (Character.isDigit(stringList[i].charAt(j))) {
+                    int n = Integer.parseInt(String.valueOf(stringList[i].charAt(j)));
+                    arrayString[n - 1] = stringList[i];
+                }
+            }
+        }
+        for (int i = 0; i < arrayString.length; i++) {
+            result.append(arrayString[i]);
+            if (i < arrayString.length - 1) {
+                result.append(" ");
+            }
+        }
+        return String.valueOf(result);
+    }
+
+    public static String solution(int n) {
+        if (n < 1 || n > 3999) {
+            throw new IllegalArgumentException("Input must be between 1 and 3999 (inclusive).");
+        }
+
+        String[] romanSymbols = {
+                "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        };
+        int[] romanValues = {
+                1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+        };
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < romanSymbols.length; i++) {
+            while (n >= romanValues[i]) {
+                result.append(romanSymbols[i]);
+                n -= romanValues[i];
+            }
+        }
+
+        return result.toString();
     }
 }
--- src/test/java/lesson/lesson22/taski/MethodsTest.java
+++ src/test/java/lesson/lesson22/taski/MethodsTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class MethodsTest {
 
@@ -16,7 +20,7 @@ class MethodsTest {
             "aa#a#a,aa",
     })
     void countTest(String s, String expected) {
-        Assertions.assertEquals(expected, Methods.count(s));
+        assertEquals(expected, Methods.count(s));
     }
 
     @ParameterizedTest
@@ -28,6 +32,55 @@ class MethodsTest {
     void twoStringEqualsTest(String s1,String s2, boolean expected) {
         String sLat1 = Methods.count(s1);
         String sLat2 = Methods.count(s2);
-        Assertions.assertEquals(expected,Methods.twoStringEquals(sLat1,sLat2));
+        assertEquals(expected,Methods.twoStringEquals(sLat1,sLat2));
+    }
+
+    @Test
+    public void testSomeDashLowerStart() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", Methods.toCamelCase(input));
+    }
+    @Test
+    public void testSomeUnderscoreLowerStart() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", Methods.toCamelCase(input));
+    }
+
+    @Test
+    public void Test1() {
+        assertEquals( 7, Methods.digital_root(16));
+    }
+    @Test
+    public void Test2() {
+        assertEquals( 6, Methods.digital_root(456));
+    }
+
+    @Test
+    public void test() {
+        assertEquals(")()())()(()()(",
+                Methods.encode("Prespecialized"));
+        assertEquals("))))())))",Methods.encode("   ()(   "));
+    }
+    @Test
+    public void test1() {
+        assertThat(Methods.order("is2 Thi1s T4est 3a"), equalTo("Thi1s is2 3a T4est"));
+    }
+
+    @Test
+    public void test2() {
+        assertThat(Methods.order("4of Fo1r pe6ople g3ood th5e the2"), equalTo("Fo1r the2 g3ood 4of th5e pe6ople"));
+    }
+
+    @Test
+    public void test3() {
+        assertThat(Methods.order(""), equalTo(""));
+    }
+    @Test
+    public void shouldConvertToRoman() {
+        assertEquals("I", Methods.solution(1));
+        assertEquals( "IV", Methods.solution(4));
+        assertEquals( "VI", Methods.solution(6));
     }
 }
