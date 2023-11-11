1c8ffc52a983644d162c14829485af13ac559e17
Aliaksei <kulevetsav@gmail.com>
2023-11-11 10:30:03 +0100

    lesson22 practice

--- src/test/java/lesson/lesson21/taski/PaymentServiceTest.java
+++ src/test/java/lesson/lesson21/taski/PaymentServiceTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
