e67e1ca2cd8517fa2632c63e83199ec41593b070
Aliaksei <kulevetsav@gmail.com>
2023-11-13 15:01:25 +0100

    hmw

--- src/test/java/lesson/lesson21/taski/OrderFulfillmentServiceTest.java
+++ src/test/java/lesson/lesson21/taski/OrderFulfillmentServiceTest.java
@@ -1,5 +1,6 @@
 package lesson.lesson21.taski;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,12 +18,25 @@ public class OrderFulfillmentServiceTest {
     @Mock
     private InventoryService inventoryService;
 
+    private Product product;
+    private int quantity;
+    private String sku;
+
+    @BeforeEach
+    public void setUp() {
+        product = new Product("123", 100);
+    }
+
+    /**
+     * Тест который проверяет метод fulfillOrder(Product product, int quantity) если все условия пройдут
+     * (stock >= quantity) == true
+     * (stockUpdated) == true
+     */
 
     @Test
-    public void fulfillOrderWithSufficientStock() {
-        Product product = new Product("123", 100);
-        String sku = product.getSku();
-        int quantity = 50;
+    public void fulfillOrderWithSufficientTrueStock() {
+        sku = product.getSku();
+        quantity = 50;
 
         Mockito.when(inventoryService.checkProductStock(sku)).thenReturn(100);
         Mockito.when(inventoryService.updateStock(sku, -quantity)).thenReturn(true);
@@ -36,51 +50,82 @@ public class OrderFulfillmentServiceTest {
         Mockito.verify(inventoryService).updateStock(sku, -quantity);
     }
 
+    /**
+     * Тест который проверяет метод fulfillOrder(Product product, int quantity) если не все условия пройдут
+     * (stock >= quantity) == false
+     */
     @Test
-    public void fulfillOrderWithFalseStock() {
-        Product product = new Product("123", 150);
-        String sku = product.getSku();
-        int quantity = -200;
+    public void fulfillOrderWithSufficientFalseStock() {
+        sku = product.getSku();
+        quantity = 50;
 
-        Mockito.when(inventoryService.checkProductStock(sku)).thenReturn(150);
+        Mockito.when(inventoryService.checkProductStock(sku)).thenReturn(100);
+        Mockito.when(inventoryService.updateStock(sku, -quantity)).thenReturn(false);
 
         boolean result = orderFulfillmentService.fulfillOrder(product, quantity);
 
         Assertions.assertFalse(result);
-        Assertions.assertEquals(150, product.getStockQuantity());
+        Assertions.assertEquals(100, product.getStockQuantity());
 
         Mockito.verify(inventoryService).checkProductStock(sku);
         Mockito.verify(inventoryService).updateStock(sku, -quantity);
     }
 
+    /**
+     * Тест который проверяет метод fulfillOrder(Product product, int quantity) если не все условия пройдут
+     * (stock >= quantity) == true
+     * (stockUpdated) == false
+     */
+
     @Test
-    public  void returnItemsToInventoryTrueTest(){
-        Product product = new Product("123", 150);
-        String sku = product.getSku();
-        int quantity = 50;
+    public void fulfillOrderWithFalseStock() {
+        sku = product.getSku();
+        quantity = 200;
+
+        Mockito.when(inventoryService.checkProductStock(sku)).thenReturn(100);
+
+        boolean result = orderFulfillmentService.fulfillOrder(product, quantity);
+
+        Assertions.assertFalse(result);
+        Assertions.assertEquals(100, product.getStockQuantity());
+
+        Mockito.verify(inventoryService).checkProductStock(sku);
+    }
+
+    /**
+     * Тест который проверяет метод returnItemsToInventory(Product product, int quantity) если условия пройдут
+     * (stockUpdated) == true
+     */
+    @Test
+    public void returnItemsToInventoryTrueTest() {
+        sku = product.getSku();
+        quantity = 50;
 
         Mockito.when(inventoryService.updateStock(sku, quantity)).thenReturn(true);
 
         boolean result = orderFulfillmentService.returnItemsToInventory(product, quantity);
 
         Assertions.assertTrue(result);
-        Assertions.assertEquals(200, product.getStockQuantity());
+        Assertions.assertEquals(150, product.getStockQuantity());
 
         Mockito.verify(inventoryService).updateStock(sku, quantity);
     }
 
+    /**
+     * Тест который проверяет метод returnItemsToInventory(Product product, int quantity) если условия пройдут
+     * (stockUpdated) == false
+     */
     @Test
-    public void returnItemsToInventoryFalseTest(){
-        Product product = new Product("123", 150);
+    public void returnItemsToInventoryFalseTest() {
         String sku = product.getSku();
-        int quantity = -150;
+        quantity = 100;
 
         Mockito.when(inventoryService.updateStock(sku, quantity)).thenReturn(false);
 
         boolean result = orderFulfillmentService.returnItemsToInventory(product, quantity);
 
         Assertions.assertFalse(result);
-        Assertions.assertEquals(150, product.getStockQuantity());
+        Assertions.assertEquals(100, product.getStockQuantity());
 
         Mockito.verify(inventoryService).updateStock(sku, quantity);
     }
