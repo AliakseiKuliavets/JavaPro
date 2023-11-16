ef6c43fc60a462543644210774d81c8b3bddcce2
Aliaksei <kulevetsav@gmail.com>
2023-11-16 18:05:38 +0100

    some

--- src/main/java/practice/leetcode/kataArrayDiff/Some.java
+++ src/main/java/practice/leetcode/kataArrayDiff/Some.java
@@ -1,6 +1,12 @@
 package practice.leetcode.kataArrayDiff;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
 public class Some {
     public static int[] arrayDiff(int[] a, int[] b) {
         StringBuilder stringBuilder = new StringBuilder();
@@ -25,6 +31,25 @@ public class Some {
 //        listA.removeAll(listB);
 //        return listA.stream().mapToInt(e -> e).toArray();
         return a;
+    }
+
+    public static boolean comp(int[] a, int[] b) {
+        if (a == null || b == null || b.length != a.length) {
+            throw new IllegalArgumentException();
+        }
+        // Create a copy of array 'a' to avoid modifying the original array
+        int[] squaredA = Arrays.copyOf(a, a.length);
+
+        // Square each element in array 'a'
+        for (int i = 0; i < squaredA.length; i++) {
+            squaredA[i] = squaredA[i] * squaredA[i];
+        }
+
+        // Sort both arrays after squaring the elements
+        Arrays.sort(squaredA);
+        Arrays.sort(b);
+
+        return Arrays.equals(squaredA, b);
     }
 
 }
--- src/test/java/practice/leetcode/kataArrayDiff/SomeTest.java
+++ src/test/java/practice/leetcode/kataArrayDiff/SomeTest.java
@@ -13,4 +13,10 @@ class SomeTest {
         assertArrayEquals(new int[]{1, 2, 2}, Some.arrayDiff(new int[]{1, 2, 2}, new int[]{}));
         assertArrayEquals(new int[]{}, Some.arrayDiff(new int[]{}, new int[]{1, 2}));
     }
+    @Test
+    public void test1() {
+        int[] a = new int[]{121, 144, 19, 161, 19, 144, 19, 11};
+        int[] b = new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361};
+        assertEquals(true, Some.comp(a, b));
+    }
 }
